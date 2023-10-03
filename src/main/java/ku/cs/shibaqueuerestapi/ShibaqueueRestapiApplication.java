973ae51749adaacc8597ffdefaaea8e4e32190c8
kankittikan <nongkan195@gmail.com>
2023-10-03 19:13:16 +0700

    first commit

---
+++ src/main/java/ku/cs/shibaqueuerestapi/ShibaqueueRestapiApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.shibaqueuerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShibaqueueRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShibaqueueRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cs/shibaqueuerestapi/ShibaqueueRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.shibaqueuerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShibaqueueRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
